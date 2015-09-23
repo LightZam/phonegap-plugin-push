476fafd4b022b7811469b51fd64258a0aa5ae1f9
Zam <love8879201@gmail.com>
2015-09-23 14:40:58 +0800

    gcm-apns support

--- src/ios/PushPlugin.h
+++ src/ios/PushPlugin.h
@@ -1,16 +1,16 @@
 /*
  Copyright 2009-2011 Urban Airship Inc. All rights reserved.
- 
+
  Redistribution and use in source and binary forms, with or without
  modification, are permitted provided that the following conditions are met:
- 
+
  1. Redistributions of source code must retain the above copyright notice, this
  list of conditions and the following disclaimer.
- 
+
  2. Redistributions in binaryform must reproduce the above copyright notice,
  this list of conditions and the following disclaimer in the documentation
  and/or other materials provided withthe distribution.
- 
+
  THIS SOFTWARE IS PROVIDED BY THE URBAN AIRSHIP INC``AS IS'' AND ANY EXPRESS OR
  IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
  MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO
@@ -26,6 +26,7 @@
 #import <Foundation/Foundation.h>
 #import <Cordova/CDV.h>
 #import <Cordova/CDVPlugin.h>
+#import <Google/CloudMessaging.h>
 
 @interface PushPlugin : CDVPlugin
 {
@@ -33,7 +34,7 @@
     BOOL    isInline;
     NSString *notificationCallbackId;
     NSString *callback;
-    
+
     BOOL ready;
 }
 
@@ -43,6 +44,19 @@
 
 @property (nonatomic, strong) NSDictionary *notificationMessage;
 @property BOOL                          isInline;
+
+// [START GCM]
+@property(nonatomic, readonly, strong) NSString *gcmSenderID;
+@property(nonatomic, assign) id apnServerOption;
+@property(nonatomic, readonly, strong) NSDictionary *registrationOptions;
+@property(nonatomic, strong) void (^registrationHandler)(NSString *registrationToken, NSError *error);
+@property(nonatomic, assign) BOOL connectedToGCM;
+@property(nonatomic, strong) NSString* registrationToken;
+@property(nonatomic, assign) BOOL subscribedToTopic;
+
+- (void)setGcmServiceState:(BOOL)state;
+- (void)onTokenRefresh;
+// [END GCM]
 
 - (void)init:(CDVInvokedUrlCommand*)command;
 - (void)unregister:(CDVInvokedUrlCommand*)command;
--- src/ios/PushPlugin.m
+++ src/ios/PushPlugin.m
@@ -1,16 +1,16 @@
 /*
  Copyright 2009-2011 Urban Airship Inc. All rights reserved.
- 
+
  Redistribution and use in source and binary forms, with or without
  modification, are permitted provided that the following conditions are met:
- 
+
  1. Redistributions of source code must retain the above copyright notice, this
  list of conditions and the following disclaimer.
- 
+
  2. Redistributions in binaryform must reproduce the above copyright notice,
  this list of conditions and the following disclaimer in the documentation
  and/or other materials provided withthe distribution.
- 
+
  THIS SOFTWARE IS PROVIDED BY THE URBAN AIRSHIP INC``AS IS'' AND ANY EXPRESS OR
  IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
  MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO
@@ -38,28 +38,31 @@
 - (void)unregister:(CDVInvokedUrlCommand*)command;
 {
     self.callbackId = command.callbackId;
-    
+
     [[UIApplication sharedApplication] unregisterForRemoteNotifications];
     [self successWithMessage:@"unregistered"];
+
+    // -- GCM
+    [self unregisterGCM];
 }
 
 - (void)init:(CDVInvokedUrlCommand*)command;
 {
     NSLog(@"Push Plugin register called");
     self.callbackId = command.callbackId;
-    
+
     NSMutableDictionary* options = [command.arguments objectAtIndex:0];
     NSMutableDictionary* iosOptions = [options objectForKey:@"ios"];
-    
+
 #if __IPHONE_OS_VERSION_MAX_ALLOWED >= 80000
     UIUserNotificationType UserNotificationTypes = UIUserNotificationTypeNone;
 #endif
     UIRemoteNotificationType notificationTypes = UIRemoteNotificationTypeNone;
-    
+
     id badgeArg = [iosOptions objectForKey:@"badge"];
     id soundArg = [iosOptions objectForKey:@"sound"];
     id alertArg = [iosOptions objectForKey:@"alert"];
-    
+
     if (([badgeArg isKindOfClass:[NSString class]] && [badgeArg isEqualToString:@"true"]) || [badgeArg boolValue])
     {
         notificationTypes |= UIRemoteNotificationTypeBadge;
@@ -67,7 +70,7 @@
         UserNotificationTypes |= UIUserNotificationTypeBadge;
 #endif
     }
-    
+
     if (([soundArg isKindOfClass:[NSString class]] && [soundArg isEqualToString:@"true"]) || [soundArg boolValue])
     {
         notificationTypes |= UIRemoteNotificationTypeSound;
@@ -75,7 +78,7 @@
         UserNotificationTypes |= UIUserNotificationTypeSound;
 #endif
     }
-    
+
     if (([alertArg isKindOfClass:[NSString class]] && [alertArg isEqualToString:@"true"]) || [alertArg boolValue])
     {
         notificationTypes |= UIRemoteNotificationTypeAlert;
@@ -83,17 +86,17 @@
         UserNotificationTypes |= UIUserNotificationTypeAlert;
 #endif
     }
-    
+
     notificationTypes |= UIRemoteNotificationTypeNewsstandContentAvailability;
 #if __IPHONE_OS_VERSION_MAX_ALLOWED >= 80000
     UserNotificationTypes |= UIUserNotificationActivationModeBackground;
 #endif
-    
+
     if (notificationTypes == UIRemoteNotificationTypeNone)
         NSLog(@"PushPlugin.register: Push notification type is set to none");
-    
+
     isInline = NO;
-    
+
 #if __IPHONE_OS_VERSION_MAX_ALLOWED >= 80000
     if ([[UIApplication sharedApplication]respondsToSelector:@selector(registerUserNotificationSettings:)]) {
         UIUserNotificationSettings *settings = [UIUserNotificationSettings settingsForTypes:UserNotificationTypes categories:nil];
@@ -107,40 +110,47 @@
     [[UIApplication sharedApplication] registerForRemoteNotificationTypes:
      (UIRemoteNotificationTypeBadge | UIRemoteNotificationTypeSound | UIRemoteNotificationTypeAlert)];
 #endif
-    
+    // -- GCM
+    [self initGCM:iosOptions];
+
     if (notificationMessage)			// if there is a pending startup notification
         [self notificationReceived];	// go ahead and process it
 }
 
 - (void)didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken {
     NSLog(@"Push Plugin register success: %@", deviceToken);
-    
+    // -- GCM
+    if (_gcmSenderID != nil) {
+        [self registerGCM];
+        return;
+    }
+
     NSMutableDictionary *results = [NSMutableDictionary dictionary];
     NSString *token = [[[[deviceToken description] stringByReplacingOccurrencesOfString:@"<"withString:@""]
                         stringByReplacingOccurrencesOfString:@">" withString:@""]
                        stringByReplacingOccurrencesOfString: @" " withString: @""];
     [results setValue:token forKey:@"deviceToken"];
-    
+
 #if !TARGET_IPHONE_SIMULATOR
     // Get Bundle Info for Remote Registration (handy if you have more than one app)
     [results setValue:[[[NSBundle mainBundle] infoDictionary] objectForKey:@"CFBundleDisplayName"] forKey:@"appName"];
     [results setValue:[[[NSBundle mainBundle] infoDictionary] objectForKey:@"CFBundleVersion"] forKey:@"appVersion"];
-    
+
     // Check what Notifications the user has turned on.  We registered for all three, but they may have manually disabled some or all of them.
 #define SYSTEM_VERSION_LESS_THAN(v) ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] == NSOrderedAscending)
-    
+
     NSUInteger rntypes;
     if (!SYSTEM_VERSION_LESS_THAN(@"8.0")) {
         rntypes = [[[UIApplication sharedApplication] currentUserNotificationSettings] types];
     } else {
         rntypes = [[UIApplication sharedApplication] enabledRemoteNotificationTypes];
     }
-    
+
     // Set the defaults to disabled unless we find otherwise...
     NSString *pushBadge = @"disabled";
     NSString *pushAlert = @"disabled";
     NSString *pushSound = @"disabled";
-    
+
     // Check what Registered Types are turned on. This is a bit tricky since if two are enabled, and one is off, it will return a number 2... not telling you which
     // one is actually disabled. So we are literally checking to see if rnTypes matches what is turned on, instead of by number. The "tricky" part is that the
     // single notification types will only match if they are the ONLY one enabled.  Likewise, when we are checking for a pair of notifications, it will only be
@@ -154,17 +164,17 @@
     if(rntypes & UIRemoteNotificationTypeSound) {
         pushSound = @"enabled";
     }
-    
+
     [results setValue:pushBadge forKey:@"pushBadge"];
     [results setValue:pushAlert forKey:@"pushAlert"];
     [results setValue:pushSound forKey:@"pushSound"];
-    
+
     // Get the users Device Model, Display Name, Token & Version Number
     UIDevice *dev = [UIDevice currentDevice];
     [results setValue:dev.name forKey:@"deviceName"];
     [results setValue:dev.model forKey:@"deviceModel"];
     [results setValue:dev.systemVersion forKey:@"deviceSystemVersion"];
-    
+
     // Send result to trigger 'registration' event but keep callback
     NSMutableDictionary* message = [NSMutableDictionary dictionaryWithCapacity:1];
     [message setObject:token forKey:@"registrationId"];
@@ -182,21 +192,21 @@
 
 - (void)notificationReceived {
     NSLog(@"Notification received");
-    
+
     if (notificationMessage && self.callbackId != nil)
     {
         NSMutableDictionary* message = [NSMutableDictionary dictionaryWithCapacity:4];
         NSMutableDictionary* additionalData = [NSMutableDictionary dictionaryWithCapacity:4];
-        
-        
+
+
         for (id key in notificationMessage) {
             if ([key isEqualToString:@"aps"]) {
                 id aps = [notificationMessage objectForKey:@"aps"];
-                
+
                 for(id key in aps) {
                     NSLog(@"Push Plugin key: %@", key);
                     id value = [aps objectForKey:key];
-                    
+
                     if ([key isEqualToString:@"alert"]) {
                         if ([value isKindOfClass:[NSDictionary class]]) {
                             for (id messageKey in value) {
@@ -229,20 +239,20 @@
                 [additionalData setObject:[notificationMessage objectForKey:key] forKey:key];
             }
         }
-        
+
         if (isInline) {
             [additionalData setObject:[NSNumber numberWithBool:YES] forKey:@"foreground"];
         } else {
             [additionalData setObject:[NSNumber numberWithBool:NO] forKey:@"foreground"];
         }
-        
+
         [message setObject:additionalData forKey:@"additionalData"];
-        
+
         // send notification message
         CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsDictionary:message];
         [pluginResult setKeepCallbackAsBool:YES];
         [self.commandDelegate sendPluginResult:pluginResult callbackId:self.callbackId];
-        
+
         self.notificationMessage = nil;
     }
 }
@@ -251,9 +261,9 @@
 {
     NSMutableDictionary* options = [command.arguments objectAtIndex:0];
     int badge = [[options objectForKey:@"badge"] intValue] ?: 0;
-    
+
     [[UIApplication sharedApplication] setApplicationIconBadgeNumber:badge];
-    
+
     NSString* message = [NSString stringWithFormat:@"app badge count set to %d", badge];
     CDVPluginResult *commandResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:message];
     [self.commandDelegate sendPluginResult:commandResult callbackId:command.callbackId];
@@ -272,8 +282,108 @@
 {
     NSString        *errorMessage = (error) ? [NSString stringWithFormat:@"%@ - %@", message, [error localizedDescription]] : message;
     CDVPluginResult *commandResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:errorMessage];
-    
+
     [self.commandDelegate sendPluginResult:commandResult callbackId:self.callbackId];
 }
+
+// [GCM START]
+
+- (void)registerGCM {
+    // Start the GGLInstanceID shared instance with the default config and request a registration token to enable reception of notifications
+    [[GGLInstanceID sharedInstance] startWithConfig:[GGLInstanceIDConfig defaultConfig]];
+    // 1 for sendbox(develop), or = 0 for production
+#ifdef __OPTIMIZE__
+    _registrationOptions = @{kGGLInstanceIDRegisterAPNSOption:deviceToken, kGGLInstanceIDAPNSServerTypeSandboxOption:@NO};
+#else
+    _registrationOptions = @{kGGLInstanceIDRegisterAPNSOption:deviceToken, kGGLInstanceIDAPNSServerTypeSandboxOption:@YES};
+#endif
+    [[GGLInstanceID sharedInstance] tokenWithAuthorizedEntity:_gcmSenderID scope:kGGLInstanceIDScopeGCM options:_registrationOptions handler:_registrationHandler];
+}
+
+// unregister gcm if exist
+- (void)unregisterGCM {
+    if (_gcmSenderID != nil) {
+        [[GGLInstanceID sharedInstance] deleteTokenWithAuthorizedEntity:_gcmSenderID scope: kGGLInstanceIDScopeGCM handler:nil];
+        [[GCMService sharedInstance] disconnect];
+    }
+}
+
+// init GCM
+- (void)initGCM:(NSMutableDictionary*)iosOptions {
+    _gcmSenderID = [iosOptions objectForKey:@"senderID"];
+    if (_gcmSenderID != nil) {
+        [[GCMService sharedInstance] startWithConfig:[GCMConfig defaultConfig]];
+
+        __weak __typeof(self) weakSelf = self;
+        // Handler for registration token request
+        _registrationHandler = ^(NSString *token, NSError *error) {
+            if (token != nil) {
+                NSLog(@"Registration Token: %@", token);
+                _registrationToken = token;
+                [weakSelf subscribeToTopic];
+
+                // Send result to trigger 'registration' event but keep callback
+                NSMutableDictionary* message = [NSMutableDictionary dictionaryWithCapacity:1];
+                [message setObject:token forKey:@"registrationId"];
+                CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsDictionary:message];
+                [pluginResult setKeepCallbackAsBool:YES];
+                [weakSelf.commandDelegate sendPluginResult:pluginResult callbackId:weakSelf.callbackId];
+            } else {
+                [weakSelf failWithMessage:@"" withError:error];
+            }
+        };
+    }
+}
+
+// change gcm connect state
+- (void)setGcmServiceState:(BOOL)state {
+    if (_gcmSenderID != nil) {
+        if (state) {
+            // Connect to the GCM server to receive non-APNS notifications
+            [[GCMService sharedInstance] connectWithHandler:^(NSError *error) {
+                if (error) {
+                    NSLog(@"Could not connect to GCM: %@", error.localizedDescription);
+                } else {
+                    self.connectedToGCM = YES;
+                    NSLog(@"Connected to GCM");
+                    [self subscribeToTopic];
+                }
+            }];
+        } else {
+            // [START disconnect_gcm_service]
+            [[GCMService sharedInstance] disconnect];
+            self.connectedToGCM = NO;
+            // [END disconnect_gcm_service]
+        }
+    }
+}
+
+- (void)subscribeToTopic {
+    NSString *const SubscriptionTopic = @"/topics/global";
+    // If the app has a registration token and is connected to GCM, proceed to subscribe to the topic
+    if (_registrationToken && _connectedToGCM) {
+        [[GCMPubSub sharedInstance] subscribeWithToken:_registrationToken topic:SubscriptionTopic options:nil handler:^(NSError *error) {
+           if (error) {
+               // Treat the "already subscribed" error more gently
+               if (error.code == 3001) {
+                   NSLog(@"Already subscribed to %@", SubscriptionTopic);
+               } else {
+                   NSLog(@"Subscription failed: %@", error.localizedDescription);
+               }
+           } else {
+               self.subscribedToTopic = true;
+               NSLog(@"Subscribed to %@", SubscriptionTopic);
+           }
+       }];
+    }
+}
+
+// A rotation of the registration tokens is happening, so the app needs to request a new token.
+- (void)onTokenRefresh {
+    NSLog(@"refresh GCM registration.");
+    [[GGLInstanceID sharedInstance] tokenWithAuthorizedEntity:_gcmSenderID scope:kGGLInstanceIDScopeGCM options:_registrationOptions handler:_registrationHandler];
+}
+
+// [GCM END]
 
 @end
